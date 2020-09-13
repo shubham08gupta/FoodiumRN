e880a31c1e1764eabefb5c22470764b8aeba4615
shubham.gupta <shubham.gupta@unifa-e.com>
2020-09-13 18:09:47 +0900

    add ios folder

---
+++ ios/Pods/Target Support Files/Pods-FoodiumRN-FoodiumRNTests/Pods-FoodiumRN-FoodiumRNTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodiumRN_FoodiumRNTests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodiumRN_FoodiumRNTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-FoodiumRN/Pods-FoodiumRN-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodiumRN : NSObject
+@end
+@implementation PodsDummy_Pods_FoodiumRN
+@end
